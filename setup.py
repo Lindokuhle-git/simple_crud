149dde58e10945e17e423fabaa00340f888f7f30
Lindokuhle-git <lindokuhlerajuili@gmail.com>
2023-08-05 15:44:17 +0200

    setup a package

---
+++ setup.py
@@ -0,0 +1,14 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='simple_crud',
+    version='1.0.0',
+    author='Lindokuhle',
+    author_email='lindokuhlerajuili@gmail.com',
+    description='''A simple database package that creates a table, 
+                   adds records in a table, 
+                   deletes a record from a table and also updates info''',
+    packages=find_packages(),
+)
