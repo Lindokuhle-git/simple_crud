1ce1fd3b60fc9cf133f3b044e9684313b6f1de32
Lindokuhle-git <lindokuhlerajuili@gmail.com>
2023-08-18 21:59:40 +0200

    created first module, still figuring out how to make test file recognize module

--- tests/test_create_db.py
+++ tests/test_create_db.py
@@ -1,40 +1,20 @@
-# test_your_module_name.py
-
 import sqlite3
 import unittest
-from CRUD.create_db import create_table
-# from CRUD import create_db, get_db, close_db, init_db
+from simple_crud import create_table, get_db, close_db, init_db
 
-
+# Define the test database name
 CONFIG_DB_NAME = "test_database.db"
-
-# Define the get_db, close_db, and init_db functions (if they are part of your module)
-def get_db():
-    return sqlite3.connect(CONFIG_DB_NAME)
-
-def close_db():
-    db = get_db()
-    db.close()
-
-def init_db():
-    db = get_db()
-    cursor = db.cursor()
-    # Initialize any necessary setup for the database here
-    db.commit()
-    db.close()
 
 class TestCreateTable(unittest.TestCase):
 
-    @classmethod
-    def setUpClass(cls):
+    def setUp(self):
         init_db()
 
-    @classmethod
-    def tearDownClass(cls):
+    def tearDown(self):
         close_db()
-    
 
     def test_create_table(self):
+        # Define the test table name and columns
         table_name = "test_table"
         columns = [
             ("column1", "INTEGER"),
@@ -53,5 +33,12 @@ class TestCreateTable(unittest.TestCase):
         self.assertIsNotNone(result, f"Table '{table_name}' does not exist in the database.")
         db.close()
 
-# if __name__ == "__main__":
-#     unittest.main()
+        # Assert that the columns exist in the table
+        cursor.execute(f"PRAGMA table_info({table_name})")
+        table_info = cursor.fetchall()
+        for col_name, col_type in columns:
+            column_exists = any(col_name == col[1] and col_type in col[2] for col in table_info)
+            self.assertTrue(column_exists, f"Column '{col_name}' with type '{col_type}' does not exist in the table '{table_name}'.")
+        
+if __name__ == "__main__":
+    unittest.main()
