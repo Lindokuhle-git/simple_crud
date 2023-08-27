67c9a1129b71d3cdb272170a5610cae8b15c1f62
Lindokuhle-git <lindokuhlerajuili@gmail.com>
2023-08-27 15:43:17 +0200

    made a new function to specifically delete a row set as a condition data

--- simple_crud/delete_db.py
+++ simple_crud/delete_db.py
@@ -24,4 +24,34 @@ def delete_table(table_name):
 
 
 def delete_row_in_table(table_name, condition_data):
-    pass        
+    """deletes the specified row from the table in a database.
+
+    Parameters:
+        table_name (str): The name of the table to delete from.
+        condition_data (dict): A dictionary containing column-value pairs to define the condition.
+
+     Example:
+        # Delete the data from table for all records where 'name' is 'Alice'
+        table_name = "employees"
+        condition_data = {"name": "Alice"}
+        delete_row_in_table(table_name, condition_data)         
+
+    Raises:
+        sqlite3.Error: If there is an error executing the SQL query.        
+
+    """
+    
+    condition_clause = ' '.join(f"{column} = ?" for column, value in condition_data.items())
+    condition_values = tuple(condition_data.values())
+
+    try:
+        db = get_db()
+        cursor = db.cursor()
+        delete_query = f'''DELETE FROM {table_name} WHERE {condition_clause}'''
+        cursor.execute(delete_query, condition_values)
+        db.commit()
+    
+    except sqlite3.Error  as e:
+        print(f"An error occurred: {e}")
+    finally:
+        close_db()          
